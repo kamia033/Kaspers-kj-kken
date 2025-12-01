47f45c59150f03e44b27d7dbb5fa5b243b450419
Kasper Lindqvist Mikkelsen <kamia033@osloskolen.no>
2025-12-01 15:50:59 +0100

    fjernet forside

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ModuleCard from './components/ModuleCard'
-import Home from './pages/home'
 import Sidebar from './components/Sidebar'
 import ChapterPage from './pages/ChapterPage'
+import './components/PaperLayout.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,15 +20,12 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={
-              <div className="content-container">
-                <h1>Kaspers Kjøkken</h1>
-                <Home />
-                <div className="ModuleCardContainer">
-                  <Link to="/fag/Matematikk%20S2/Sannsynlighet/Introduksjon" style={{ textDecoration: 'none' }}>
-                    <ModuleCard title="S2: Sannsynlighet" image={reactLogo} />
-                  </Link>
-                  <ModuleCard title="Informasjonsteknologi" image={reactLogo} />
-                  <ModuleCard title="Test Modul" image={reactLogo} />
+              <div className="paper-container">
+                <div className="paper-sheet">
+                  <h1 className="paper-title">Velkommen</h1>
+                  <div className="paper-content">
+                    <p>Velg et emne fra menyen til venstre for å komme i gang.</p>
+                  </div>
                 </div>
               </div>
             } />
